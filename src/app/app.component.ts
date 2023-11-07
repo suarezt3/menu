397337a3e43590c99053395b0352f28be3ccb5b9
Eyder Suarez <ey_der19@hotmail.com>
2023-11-07 17:29:30 -0500

    Menu hamburguesa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
 
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'menu';
+export class AppComponent implements OnInit {
+
+
+
+constructor(private elementRef: ElementRef) {
+
+
+}
+
+ngOnInit() {
+
+  const nav = this.elementRef.nativeElement.querySelector('#nav');
+  const abrir = this.elementRef.nativeElement.querySelector('#abrir');
+  const cerrar = this.elementRef.nativeElement.querySelector('#cerrar');
+
+  abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+  });
+
+  cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+  });
+}
+
 }
