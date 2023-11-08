110bc069bc35f00167e86b76b55ab67bba63780d
Eyder Suarez <ey_der19@hotmail.com>
2023-11-08 16:54:35 -0500

    Se hacer refactor del codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,26 +8,37 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
+public nav: any;
+// public abrir: any;
+// public cerrar: any;
 
 
-constructor(private elementRef: ElementRef) {
+constructor(private elementRef: ElementRef) {}
 
 
-}
 
 ngOnInit() {
 
-  const nav = this.elementRef.nativeElement.querySelector('#nav');
-  const abrir = this.elementRef.nativeElement.querySelector('#abrir');
-  const cerrar = this.elementRef.nativeElement.querySelector('#cerrar');
+ this.nav = this.elementRef.nativeElement.querySelector('#nav');
+  // const abrir = this.elementRef.nativeElement.querySelector('#abrir');
+  // const cerrar = this.elementRef.nativeElement.querySelector('#cerrar');
 
-  abrir.addEventListener('click', () => {
-    nav.classList.add('visible');
-  });
+  // abrir.addEventListener('click', () => {
+  //   nav.classList.add('visible');
+  // });
 
-  cerrar.addEventListener('click', () => {
-    nav.classList.remove('visible');
-  });
-}
+  // cerrar.addEventListener('click', () => {
+  //   nav.classList.remove('visible');
+  // });
+
+ }
+
+ abrir() {
+  this.nav.classList.add('visible');
+ }
+
+ cerrar() {
+  this.nav.classList.remove('visible')
+ }
 
 }
